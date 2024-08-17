739ebcd8764518a150588cd6ce6d73ecfbde2553
ngullotta <nikolaus.gullotta@protonmail.com>
2024-08-17 14:47:31 -0500

    Start work on timer system to clean things up

--- scenes/test/CharacterBody3D.gd
+++ scenes/test/CharacterBody3D.gd
@@ -9,41 +9,46 @@ enum State {
     SLIDING = 0x20
 }
 
-# State tracking
-@export var state = 0
-var last_velocity = Vector3.ZERO
-var last_direction = Vector3.ZERO
-var last_camera_aim = Vector3.ZERO
-var player_default_rota = Vector3.ZERO
-var camera_default_rota = Vector3.ZERO
-
+# References
 @onready var camera = $Camera3D
 @onready var dash_rect = $Camera3D/ColorRect
 @onready var shield = $Shield
+
+# State
+@export var state = 0
+var last_velocity = Vector3.ZERO
+var last_direction = Vector3.ZERO
+var last_camera_rota = Vector3.ZERO
+
+# Defaults
+var player_default_rota = Vector3.ZERO
 var shield_default_pos = Vector3.ZERO
-var shield_default_rot = Vector3.ZERO
+var shield_default_rota = Vector3.ZERO
+
+# Shield stuff
+const SHIELD_FORCE = 20
 var shield_target_pos = Vector3.ZERO
 var shield_target_rota = Vector3.ZERO
-
-const SPEED = 15.0
-const JUMP_VELOCITY = 7.0
-const DASH_FORCE = 200
-const SHIELD_FORCE = 20
-const SLIDE_FORCE = 2
 
 # Camera Stuff
 @export var CAMERA_SENSITIVITY = 0.1
 
 # Jump Stuff
-@export var jump_counter = 0
+@export var JUMP_VELOCITY = 7
+var jump_counter = 0
+
+# Physics Stuff
+const SPEED = 15.0
 
 # Dash stuff
 @export var dash_acceleration = 9
-@export var dash_speed = 1
-@export var dash_counter = 0
+@export var DASH_FORCE = 200
+var dash_speed = 1
+var dash_counter = 0
 var dash_target = Vector3.ZERO
 
 # Slide stuff
+@export var SLIDE_FORCE = 2
 var slide_target = Vector3.ZERO
 
 # Timers
@@ -58,14 +63,86 @@ var slide_time = 1.5
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
+var timers = {}
+
 func set_active_state(value, yn := true):
     if yn:
         state |= value
     else:
         state &= ~value
 
+func remove_active_state(value):
+    set_active_state(value, false)
+
 func has_active_state(value):
     return bool(state & value)
+    
+func add_timer(
+    label,
+    start_value, 
+    end_value := 0, 
+    delta := 0, 
+    state := 0, 
+    callbacks := []
+):
+    # Add a timer to `timers`
+    # every time `process_timers` is called, the `current_value` will be 
+    # decremented by `delta` until the `current_value` is < `end_value`
+    # after this happens a callback can be triggered.
+    #
+    # If no value provided for delta, whatever is passed to `process_ticks` 
+    # will be used. If `state` is provided, `state` will be set to active, 
+    # and then removed once the `current_value` is < `end_value`
+    timers[label] = {
+        "current_value": start_value,
+        "end_value": end_value,
+        "delta": delta,
+        "state": state,
+        "callbacks": callbacks
+    }
+    
+func cleanup_timers():
+    for timer in timers:
+        var data = timers[timer] 
+        if data["current_value"] < data["end_value"]:
+            timers.erase(data)
+
+func process_timers(delta):
+    for timer in timers:
+        var data = timers[timer]
+        if not has_active_state(data["state"]):
+            set_active_state(data["state"])
+        data["current_value"] = data["current_value"] - delta
+        if data["current_value"] < data["end_value"]:
+            for callback in data["callbacks"]:
+                callback.call()
+            remove_active_state(data["state"])
+
+    #dash_timer -= delta
+    #slide_timer -= delta
+    #if dash_timer < 0 and has_active_state(State.DASHING):
+        #dash_rect.set_visible(false)
+        #dash_timer = 0
+        #set_active_state(State.DASHING, false)
+#
+    #if slide_timer < 0 and has_active_state(State.SLIDING):
+        #rotation = player_default_rota
+        #dash_rect.set_visible(false)
+        #slide_timer = 0
+        #set_active_state(State.SLIDING, false)
+        #camera.rotation.x += deg_to_rad(90)
+
+func do_dash():
+    add_timer(
+        "dash",
+        dash_time,
+        0,
+        0,
+        State.DASHING | State.SHIELDING,
+        [
+            func(): dash_rect.set_visible(false)
+        ]
+    )    
 
 func _state_to_label():
     var labels = []
@@ -83,29 +160,13 @@ func _state_to_label():
         labels.append("Sliding")
     return " | ".join(labels)
 
-func process_ticks(delta):
-    dash_timer -= delta
-    slide_timer -= delta
-    if dash_timer < 0 and has_active_state(State.DASHING):
-        dash_rect.set_visible(false)
-        dash_timer = 0
-        set_active_state(State.DASHING, false)
-        
-    if slide_timer < 0 and has_active_state(State.SLIDING):
-        rotation = player_default_rota
-        dash_rect.set_visible(false)
-        slide_timer = 0
-        set_active_state(State.SLIDING, false)
-        
-
 func _ready():
     Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
     set_process_input(true)
     dash_rect.set_visible(false)
     player_default_rota = rotation
-    camera_default_rota = rotation
     shield_default_pos = shield.position
-    shield_default_rot = shield.rotation
+    shield_default_rota = shield.rotation
 
 func _physics_process(delta):
     # Add the gravity.
@@ -146,49 +207,47 @@ func _physics_process(delta):
     # Get the input direction and handle the movement/deceleration.
     # As good practice, you should replace UI actions with custom gameplay actions.
     var input_dir = Input.get_vector(
-        "ui_left", 
-        "ui_right", 
-        "ui_up", 
+        "ui_left",
+        "ui_right",
+        "ui_up",
         "ui_down"
     )
     var direction = (
         transform.basis * Vector3(input_dir.x, 0, input_dir.y)
     ).normalized()
-    
+
     if Input.is_action_just_released("shift_left") or Input.is_action_just_released("mouse_rclick"):
         set_active_state(State.SHIELDING, false)
 
     # Dash Handling (speedup)
     var speed_mult = 1
     if Input.is_action_just_pressed("shift_left"):
-        set_active_state(State.SHIELDING, true)
+        #set_active_state(State.SHIELDING, true)
         shield_target_pos = camera.position
         shield_target_pos.z -= 0.5 # put it a little in front
         shield_target_pos.y -= 1.10 # put it a little down
-        shield_target_rota = shield_default_rot
+        shield_target_rota = shield_default_rota
         shield_target_rota.y = deg_to_rad(90)
-        if dash_counter == 0:
-            speed_mult = 10
-            dash_timer = dash_time
-            dash_rect.set_visible(true)
-            set_active_state(State.DASHING)
-            var aim = camera.get_camera_transform()
-            if direction == Vector3.ZERO:
-                dash_target = -aim.basis.z * 50
-                
-                
+        dash_rect.set_visible(true)
+        speed_mult = 10
+        var aim = camera.get_camera_transform()
+        if direction == Vector3.ZERO:
+            dash_target = -aim.basis.z * 50
+        do_dash()
+
+
     if Input.is_action_just_pressed("mouse_rclick"):
         set_active_state(State.SHIELDING, true)
         shield_target_pos = camera.position
         shield_target_pos.z -= 0.5 # put it a little in front
         shield_target_pos.y -= 1.10 # put it a little down
-        shield_target_rota = shield_default_rot
+        shield_target_rota = shield_default_rota
         shield_target_rota.y = deg_to_rad(90)
-    
+
     if has_active_state(State.SHIELDING):
         shield.position = lerp(
-            shield.position, 
-            shield_target_pos, 
+            shield.position,
+            shield_target_pos,
             SHIELD_FORCE * delta
         )
         shield.rotation = lerp(
@@ -198,65 +257,70 @@ func _physics_process(delta):
         )
     else:
         shield.position = lerp(
-            shield.position, 
-            shield_default_pos, 
+            shield.position,
+            shield_default_pos,
             (SHIELD_FORCE * 0.25) * delta
         )
         shield.rotation = lerp(
             shield.rotation,
-            shield_default_rot,
+            shield_default_rota,
             (SHIELD_FORCE * 0.25) * delta
         )
 
     if direction != Vector3.ZERO:
         velocity.x = direction.x * SPEED
         velocity.z = direction.z * SPEED
-        set_active_state(State.SLIDING, false)
         slide_timer = -1
     else:
         # Idle -> Dash
         if has_active_state(State.DASHING):
             velocity = lerp(
-                last_velocity, 
-                dash_target * dash_speed, 
+                last_velocity,
+                dash_target * dash_speed,
                 dash_acceleration * delta
             )
         # Idle -> Slide
         elif has_active_state(State.SLIDING):
             velocity = lerp(
-                last_velocity, 
-                slide_target * SLIDE_FORCE, 
+                last_velocity,
+                slide_target * SLIDE_FORCE,
                 dash_acceleration * delta
             )
+            #camera.rotation.x = lerp(
+                #last_camera_rota.x,
+                #deg_to_rad(-82),
+                #5 * delta
+            #)
             rotation.x = deg_to_rad(82)
-            camera.rotation.x = deg_to_rad(-90)
         else:
             velocity.x = move_toward(velocity.x, 0, SPEED)
             velocity.z = move_toward(velocity.z, 0, SPEED)
- 
+
     # Dashing Logic
     if has_active_state(State.DASHING) and direction != Vector3.ZERO:
         velocity.x += lerp(
-            velocity.x, 
-            last_velocity.x * dash_speed, 
+            velocity.x,
+            last_velocity.x * dash_speed,
             dash_acceleration * delta
         )
         velocity.z += lerp(
-            velocity.z, 
-            last_velocity.z * dash_speed, 
+            velocity.z,
+            last_velocity.z * dash_speed,
             dash_acceleration * delta
         )
-        
+
     last_velocity = velocity
     last_direction = direction
+    last_camera_rota = camera.rotation
     move_and_slide()
-    process_ticks(delta)
-    
+    process_timers(delta)
+    cleanup_timers()
+
     if velocity == Vector3.ZERO and state == 0:
         set_active_state(State.IDLE)
     elif velocity != Vector3.ZERO and state > 0:
         set_active_state(State.IDLE, false)
-    
+
     # Debug
     var labelstate = _state_to_label()
     $Camera3D/RichTextLabel.text = "[center]%s[/center]" % labelstate
